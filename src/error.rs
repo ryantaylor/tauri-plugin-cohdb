80cdb23b5689a71c792ed70734ab9ca36f243d2b
ryantaylor <2320507+ryantaylor@users.noreply.github.com>
2024-02-12 20:33:07 -0500

    Update errors with interpolated strings.

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,13 @@
 #[derive(thiserror::Error, Debug)]
 #[non_exhaustive]
 pub enum Error {
-    #[error("There was an error opening your browser")]
+    #[error("There was an error opening your browser: {0}")]
     Shell(#[from] tauri::api::Error),
-    #[error("There was an error retrieving your token")]
+    #[error("There was an error retrieving your token: {0}")]
     TokenRequest(String),
-    #[error("There was an error accessing your keyring")]
+    #[error("There was an error accessing your keyring: {0}")]
     Keyring(#[from] keyring::Error),
-    #[error("There was an error with the cohdb API")]
+    #[error("There was an error with the cohdb API: {0}")]
     Http(#[from] reqwest::Error),
     #[error("Please connect your cohdb account first")]
     Unauthenticated,
